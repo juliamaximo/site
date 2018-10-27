59599efd220f06a19ad09c66da9e12c647ceed6a
juliamaximo <juliamaximojulia@gmail.com>
2018-10-26 21:56:06 -0300

    Minha alteração 1

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double volume, raio, altura;
+            const Double PI = 3.14159;
+
+            Console.Write(" Informe o valor do raio: ");
+            raio = double.Parse(Console.ReadLine());
+
+
+            Console.Write(" Informe a altura: ");
+            altura = double.Parse(Console.ReadLine());
+
+            volume = PI * Math.Pow(raio,2) * altura;
+
+            Console.Write(" O volume da lata é :" + volume);
+            
+
+        }
+    }
+}
